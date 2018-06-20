cee631d8409f9bd37aa9fa5bf2dd7d39dd6a6088
Justine Bricknell <justinegayton@gmail.com>
2018-06-20 21:50:33 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,9 +1,10 @@
 def countdown(number)
 while number > 0
   number -= 1
-    return "#{number} SECOND(S)!"
-  end
-  return "HAPPY NEW YEAR!"
+  puts"#{number} SECOND(S)!"
+end
+  number == 0
+  puts "HAPPY NEW YEAR!"
 end
 
 
