725da16d5ca1932a82e38218fd492e09ea8e2dd9
Justine Bricknell <justinegayton@gmail.com>
2018-06-20 21:46:30 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,13 @@
-#write your code here
+def countdown(number)
+while
 
-def countdown
+    return "#{number} SECOND(S)!"
+  end
+  return "HAPPY NEW YEAR!"
+end
+
+
+
+def countdown_with_sleep(integer)
+  integer.sleep(5_secs)
 end
